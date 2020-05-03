0c77faeb8a328411ec0b32ad61efd31553adca0e
TorTales <31913651+TTeig@users.noreply.github.com>
2020-05-03 12:51:47 +1000

    stuff

--- Assets/Scripts/ButtonChangeScene.gd
+++ Assets/Scripts/ButtonChangeScene.gd
@@ -5,8 +5,10 @@ export var Scene := "res://Assets/Scenes/Game.tscn"
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
+func _input(ev):
+	if ev is InputEventKey and ev.scancode == KEY_SPACE:
+		get_tree().change_scene(Scene)
+		
 func _pressed():
 	get_tree().change_scene(Scene)
 
