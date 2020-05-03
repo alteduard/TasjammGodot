7f55216fe5bb2d73e47e766cad38a1b8961b6408
TorTales <31913651+TTeig@users.noreply.github.com>
2020-05-03 11:19:21 +1000

    cameracontroller

---
+++ Assets/Scripts/CameraController.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+export(NodePath) var player1
+export(NodePath) var player2
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	var p1 = get_node(player1)
+	var p2 = get_node(player2)
+
+	global_position.x = p1.global_position.x + (p2.global_position.x - p1.global_position.x) * 0.5
